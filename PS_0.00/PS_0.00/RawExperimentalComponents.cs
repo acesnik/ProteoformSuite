1d479fe619fa6e246f38ba1c65895310b7e10389
Anthony <anthony.cesnik@gmail.com>
2016-05-10 16:00:41 -0500

    references in RawExptComponents

--- PS_0.00/PS_0.00/RawExperimentalComponents.cs
+++ PS_0.00/PS_0.00/RawExperimentalComponents.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SQLite;
+using System.Data.Linq;
 using System.Data.OleDb;
 using System.Drawing;
 using System.IO;
